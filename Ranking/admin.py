ea20f49d81cae244f414bb24e00849d3344167a2
JafethDC <loguepw@gmail.com>
2014-11-21 21:00:56 -0500

    Viendo codificacion

--- Ranking/admin.py
+++ Ranking/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
 from Ranking.models import *
 
-#class CursoDictadoInLine(admin.TabularInline):
-#	model = CursoDictado
+class CursoDictadoInLine(admin.TabularInline):
+	model = Dictado
 
-#class ProfesorAdmin(admin.ModelAdmin):
-#	fieldsets = [
-#		('Nombre',{'fields':[('nombre')]}),
-#	]
-#	inlines = [CursoDictadoInLine]
+class ProfesorAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Nombre',{'fields':[('nombre')]}),
+	]
+	inlines = [CursoDictadoInLine]
 
-#admin.site.register(Profesor, ProfesorAdmin)
-admin.site.register(Profesor)
-admin.site.register(CursoDictado)
-admin.site.register(Estudiante)
+
+class ComentarioInLine(admin.TabularInline):
+	model = Comentario
+
+class EstudianteAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Nombre',{'fields':[('nombre')]}),
+	]
+	inlines = [ComentarioInLine]
+
+admin.site.register(Profesor,ProfesorAdmin)
+admin.site.register(Curso)
+admin.site.register(Estudiante,EstudianteAdmin)
--- Ranking/models.py
+++ Ranking/models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 class Profesor(models.Model):
 	nombre = models.CharField(max_length = 70)
+	exigenciaP = models.DecimalField(max_digits=4,decimal_places=2,null=True)#Why null true :(
+	pedagogiaP = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+	conocimientoP = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+	pasabilidadP = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+	#Comin soon : to be continued
 	def __unicode__(self):
 		return self.nombre
 
 class Curso(models.Model):
 	nombre = models.CharField(max_length = 70)
 	ciclo = models.IntegerField()
+	escuela = models.CharField(max_length = 20,null=True)
 	profesores = models.ManyToManyField(Profesor, through='Dictado')
 	def __unicode__(self):
 		return self.nombre
@@ -16,31 +22,31 @@ class Curso(models.Model):
 class Dictado(models.Model):
 	curso = models.ForeignKey(Curso)
 	profesor = models.ForeignKey(Profesor)
-	semestre = IntegerField() #El de 5 digitos : 20142
-	def __unicode__(self):
-		return self.curso+" - "+self.profesor+" - "+self.semestre
+	semestre = models.IntegerField() #El de 5 digitos : 20142
 
-#class CursoDictado(models.Model):
-#	nombre = models.CharField(max_length = 70)
-#	ciclo = models.IntegerField() #Cuidado debe tener 5 digitos <ano><semestre>
-#	profesores = models.ManyToManyField(Profesor)
-#
-#	def __unicode__(self):
-#		return self.nombre
+	exigenciaP = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+	pedagogiaP = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+	conocimientoP = models.DecimalField(max_digits=4,decimal_places=2,null=True)	
+	pasabilidadP = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+
+	def __unicode__(self):
+		return str(self.curso)+" - "+str(self.semestre /10)+" "+str(self.semestre%10)
 
 class Estudiante(models.Model):
 	nombre = models.CharField(max_length = 70)
 	#Codigo?
-	escuela = models.CharField(max_length = 70) #Podría ser solo SW o S
-
 	def __unicode__(self):
 		return self.nombre
 
 class Comentario(models.Model):
 	contenido = models.CharField(max_length = 1000)
-	cursoDictado = models.ForeignKey(Dictado)
 	autor = models.ForeignKey(Estudiante)  
 	fecha = models.DateTimeField('Fecha de comentario')
+	cursoDictado = models.ForeignKey(Dictado, null=True)
 
+	exigencia = models.IntegerField(default=0)
+	pedagogia = models.IntegerField(default=0)
+	conocimiento = models.IntegerField(default=0)
+	pasabilidad = models.IntegerField(default=0)
 	def __unicode__(self):
-		return self.autor+" - "+self.fecha
+		return str(self.autor)+" - "+str(self.fecha)
